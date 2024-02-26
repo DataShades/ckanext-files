c76d5ffc0bde5731eb820bf9f4fb262965be5120
mutantsan <kvaqich@gmail.com>
2024-02-26 10:27:30 +0200

    fix: fix auth functions

--- ckanext/files/logic/auth.py
+++ ckanext/files/logic/auth.py
@@ -5,24 +5,24 @@ auth, get_auth_functions = Collector("files").split()
 
 @auth
 def file_create(context, data_dict):
-    {"success": False}
+    return {"success": False}
 
 
 @auth
 def file_update(context, data_dict):
-    {"success": False}
+    return {"success": False}
 
 
 @auth
 def file_delete(context, data_dict):
-    {"success": False}
+    return {"success": False}
 
 
 @auth
 def file_show(context, data_dict):
-    {"success": False}
+    return {"success": True}
 
 
 @auth
 def get_unused_files(context, data_dict):
-    {"success": False}
+    return {"success": False}

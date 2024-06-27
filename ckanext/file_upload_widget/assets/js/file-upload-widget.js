924b5dfae4a21ef46451dfaf10d3a48d872f1d32
mutantsan <kvaqich@gmail.com>
2024-06-27 13:58:36 +0300

    working on a widget, part 4

--- ckanext/file_upload_widget/assets/js/file-upload-widget.js
+++ ckanext/file_upload_widget/assets/js/file-upload-widget.js
@@ -29,33 +29,36 @@ ckan.module("file-upload-widget", function ($, _) {
             this.fileSelectBtn = this.el.find('.btn-file-select');
             this.cancelFileSelectBtn = this.el.find('.btn-cancel-file-select');
             this.dropZoneArea = this.el.find(this.const.dropZone);
+            this.fileInput = this.el.find('input[type="file"]');
 
             this.cancelBtn = this.el.find(this.const.cancelBtn);
 
             // Bind events
-            this.fileInputBtn.on('click', this._onFileInputTriggered);
-            this.urlInputBtn.on('click', this._onUrlInputTriggered);
-            this.mediaInputBtn.on('click', this._onMediaInputTriggered);
-            this.cancelBtn.on('click', this._onCancelAction);
+            this.fileInputBtn.on("click", this._onFileInputTriggered);
+            this.urlInputBtn.on("click", this._onUrlInputTriggered);
+            this.mediaInputBtn.on("click", this._onMediaInputTriggered);
+            this.cancelBtn.on("click", this._onCancelAction);
             this.fileSearchInput.on('input', this._onFileSearch);
             this.el.find('li.files--file-item input').on('change', this._onFileSelect);
-            this.cancelFileSelectBtn.on('click', this._onCancelFileSelect);
+            this.cancelFileSelectBtn.on("click", this._onCancelFileSelect);
+            this.fileInput.on('change', this._onFileSelected);
 
             // Dropzone events
-            // Prevent default drag behaviors
             this.dropZoneArea.on("drop", this._onDropFile);
 
-            ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-                this.dropZoneArea.on(eventName, this._preventDefaults)
+            // Prevent default drag behaviors
+            ;["dragenter", "dragover", "dragleave", "drop"].forEach(eventName => {
+                this.dropZoneArea.on(eventName, this._preventDropDefaults)
                 $(document.body).on(eventName, this._preventDropDefaults)
             });
 
             // Highlight drop area when item is dragged over it
-            ;['dragenter', 'dragover'].forEach(eventName => {
+            ;["dragenter", "dragover"].forEach(eventName => {
                 this.dropZoneArea.on(eventName, this._highlightDropZone)
             });
 
-            ;['dragleave', 'drop'].forEach(eventName => {
+            // Unhighlight drop area when item is dragged out of it
+            ;["dragleave", "drop"].forEach(eventName => {
                 this.dropZoneArea.on(eventName, this._unhighlightDropZone)
             })
         },
@@ -222,6 +225,16 @@ ckan.module("file-upload-widget", function ($, _) {
 
         _onDropFile: function (e) {
             console.log(e.originalEvent.dataTransfer.files)
+        },
+
+        _onFileSelected: function (e) {
+            Array.from(e.target.files).forEach(file => {
+                this._handleFile(file);
+            })
+        },
+
+        _handleFile(file) {
+            console.log(file)
         }
     };
 });

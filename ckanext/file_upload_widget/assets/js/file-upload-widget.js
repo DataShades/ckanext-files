b0f20428e763bae1475e72a4f7ffa4e7556bcee1
mutantsan <kvaqich@gmail.com>
2024-07-01 13:17:16 +0300

    working on a widget, part 5

--- ckanext/file_upload_widget/assets/js/file-upload-widget.js
+++ ckanext/file_upload_widget/assets/js/file-upload-widget.js
@@ -1,3 +1,7 @@
+$.fn.isValid = function () {
+    return this[0].checkValidity()
+}
+
 ckan.module("file-upload-widget", function ($, _) {
     "use strict";
 
@@ -9,13 +13,46 @@ ckan.module("file-upload-widget", function ($, _) {
             mainWindowBlock: '.fuw-main-window',
             urlInputBlock: '.fuw-url-input',
             mediaInputBlock: '.fuw-media-input',
+            selectedBlock: '.fuw-selected-files',
             cancelBtn: '.fuw-cancel-btn',
-            dropZone: '.fuw-main-window__dropzone'
+            dropZone: '.fuw-main-window__dropzone',
+            selectedFiles: '.fuw-selected-files--list',
+        },
+        options: {
+            instanceId: null,
+        },
+        templates: {
+            selectedFileItem: (fileId, fileName, fileSize) => `
+                <li class="fuw-selected-files--file-item-wrapper fuw-selected-file" fuw-file-id="${fileId}">
+                    <div class="fuw-selected-files--file-preview">
+                        <div class="file-tile--file-icon">
+                            <svg aria-hidden="true" focusable="false" width="25" height="25" viewBox="0 0 25 25">
+                                <g fill="#A7AFB7" fill-rule="nonzero">
+                                    <path d="M5.5 22a.5.5 0 0 1-.5-.5v-18a.5.5 0 0 1 .5-.5h10.719a.5.5 0 0 1 .367.16l3.281 3.556a.5.5 0 0 1 .133.339V21.5a.5.5 0 0 1-.5.5h-14zm.5-1h13V7.25L16 4H6v17z"></path>
+                                    <path d="M15 4v3a1 1 0 0 0 1 1h3V7h-3V4h-1z"></path>
+                                </g>
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="fuw-selected-files--file-info">
+                        <div class="fuw-selected-files--file-name">File ${fileName}</div>
+                        <div class="fuw-selected-files--file-size">${fileSize}</div>
+                    </div>
+                    <i class="fa-solid fa-times file-tile--file-remove"></i>
+                </li>
+            `
         },
         initialize: function () {
             $.proxyAll(this, /_/);
 
-            console.log('file-upload-widget initialized');
+            if (!this.options.instanceId) {
+                log.error("Widget instance ID is required");
+                return
+            };
+
+            this.lsSelectedFilesKey = 'fuw-selected-files:' + this.options.instanceId,
+
+                this._clearStoredData();
 
             this.fileInputBtn = this.el.find(this.const.fileInputButton);
             this.urlInputBtn = this.el.find(this.const.urlInputButton);
@@ -24,12 +61,15 @@ ckan.module("file-upload-widget", function ($, _) {
             this.mainWindow = this.el.find(this.const.mainWindowBlock);
             this.urlWindow = this.el.find(this.const.urlInputBlock);
             this.mediaWindow = this.el.find(this.const.mediaInputBlock);
+            this.selectionWindow = this.el.find(this.const.selectedBlock);
 
             this.fileSearchInput = this.el.find('#fuw-media-input--search');
             this.fileSelectBtn = this.el.find('.btn-file-select');
             this.cancelFileSelectBtn = this.el.find('.btn-cancel-file-select');
             this.dropZoneArea = this.el.find(this.const.dropZone);
             this.fileInput = this.el.find('input[type="file"]');
+            this.selectedFilesContainer = this.el.find(this.const.selectedFiles);
+            this.urlImportBtn = this.urlWindow.find(".btn-url-import");
 
             this.cancelBtn = this.el.find(this.const.cancelBtn);
 
@@ -42,6 +82,10 @@ ckan.module("file-upload-widget", function ($, _) {
             this.el.find('li.files--file-item input').on('change', this._onFileSelect);
             this.cancelFileSelectBtn.on("click", this._onCancelFileSelect);
             this.fileInput.on('change', this._onFileSelected);
+            this.urlImportBtn.on('click', this._onUrlImport);
+
+            // Bind events on non existing elements
+            $("body").on("click", ".file-tile--file-remove", this._onRemoveSelectedFile);
 
             // Dropzone events
             this.dropZoneArea.on("drop", this._onDropFile);
@@ -82,7 +126,7 @@ ckan.module("file-upload-widget", function ($, _) {
          * @param {Event} e
          */
         _onUrlInputTriggered: function (e) {
-            this.urlWindow.toggle();
+            this._enableUrlWindow();
             this.mainWindow.toggle();
             this.cancelBtn.toggle();
         },
@@ -109,10 +153,22 @@ ckan.module("file-upload-widget", function ($, _) {
          * @param {Event} e
          */
         _onCancelAction: function (e) {
-            this.urlWindow.hide();
+            this._disableUrlWindow();
+
             this.mediaWindow.hide();
+            this.selectionWindow.hide();
             this.mainWindow.toggle();
             this.cancelBtn.toggle();
+        },
+
+        _enableUrlWindow: function () {
+            this.urlWindow.find("input").prop("disabled", false);
+            this.urlWindow.show();
+        },
+
+        _disableUrlWindow: function () {
+            this.urlWindow.find("input").prop("disabled", true);
+            this.urlWindow.hide();
         },
 
         /**
@@ -224,7 +280,9 @@ ckan.module("file-upload-widget", function ($, _) {
         },
 
         _onDropFile: function (e) {
-            console.log(e.originalEvent.dataTransfer.files)
+            Array.from(e.originalEvent.dataTransfer.files).forEach(file => {
+                this._handleFile(file);
+            });
         },
 
         _onFileSelected: function (e) {
@@ -233,8 +291,145 @@ ckan.module("file-upload-widget", function ($, _) {
             })
         },
 
-        _handleFile(file) {
-            console.log(file)
+        _handleFile: function (file) {
+            // TODO: make an ID instead of using the file name
+            this._addFileItem(file.name, file.name, file.size);
+
+            this.urlWindow.hide();
+            this.mediaWindow.hide();
+            this.selectionWindow.show();
+        },
+
+        _onUrlImport: function (e) {
+            let fileUrl = this.urlWindow.find("input").val();
+
+            if (fileUrl.length && this.urlWindow.find("input").isValid()) {
+                this.urlWindow.find("input").val("");
+                this._handleFile({ name: fileUrl, size: 0 });
+            } else {
+                this.urlWindow.find("input").focus();
+                this.urlWindow.find("input").get(0).reportValidity();
+            }
+
+        },
+
+        _addFileItem: function (fileId, fileName, fileSize) {
+            const files = this.getDataFromLocalStorage(this.lsSelectedFilesKey) || [];
+            const fileItem = $(this.templates.selectedFileItem(
+                fileId, this._truncateFileName(fileName), this._formatFileSize(fileSize))
+            );
+
+            for (const file of files) {
+                console.log(file);
+                if (file.name === fileName) {
+                    alert("File already selected");
+                    return;
+                }
+            }
+
+            files.push({ id: fileId, name: fileName, size: fileSize });
+
+            this.storeDataInLocalStorage(this.lsSelectedFilesKey, files);
+            this.selectedFilesContainer.append(fileItem);
+        },
+
+        /**
+         * Format file size in human readable format
+         *
+         * @param {Number} bytes
+         *
+         * @returns {String} formatted file size
+         */
+        _formatFileSize: function (bytes) {
+            if (bytes === 0) return '0 Bytes';
+            const k = 1024;
+            const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+            const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+            return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+        },
+
+        /**
+         * Truncate file name if it is longer than the maximum length
+         *
+         * @param {String} fileName - file name
+         * @param {Number} maxLength - maximum length of the file name
+         *
+         * @returns {String} truncated file name
+         */
+        _truncateFileName: function (fileName, maxLength = 35) {
+            // Ensure the file name is longer than the maximum length
+            if (fileName.length <= maxLength) {
+                return fileName;
+            }
+
+            // Calculate lengths for the beginning and end parts
+            const keepLength = Math.floor((maxLength - 3) / 2);
+            const start = fileName.substring(0, keepLength);
+            const end = fileName.substring(fileName.length - keepLength);
+
+            // Return the truncated file name with ellipsis
+            return `${start}...${end}`;
+        },
+
+        _onRemoveSelectedFile: function (e) {
+            let fileEl = $(e.target).closest(".fuw-selected-file");
+
+            let files = this.getDataFromLocalStorage(this.lsSelectedFilesKey) || [];
+            files = files.filter(file => file.id !== fileEl.attr("fuw-file-id"));
+
+            fileEl.remove();
+
+            this.storeDataInLocalStorage(this.lsSelectedFilesKey, files);
+            this._toggleFileSelectionWindow(this._calculateSelectedFilesNum() > 0);
+        },
+
+        _calculateSelectedFilesNum: function () {
+            return this.el.find(".fuw-selected-file").length;
+        },
+
+        _toggleFileSelectionWindow: function (flag) {
+            this.selectionWindow.toggle(flag);
+
+            if (!flag) {
+                this.mainWindow.show();
+                this.cancelBtn.hide();
+            }
+        },
+
+        /**
+         * Get data from a local storage
+         *
+         * @param {String} key - key to get data from local storage
+         * @returns {Object | null} - data from local storage
+         */
+        getDataFromLocalStorage: function (key) {
+            const jsonString = localStorage.getItem(key);
+
+            if (jsonString) {
+                return JSON.parse(jsonString);
+            }
+
+            return null;
+        },
+
+        /**
+         * Store data in local storage
+         *
+         * @param {String} key - key to store data in a local storage
+         * @param {Object | Array} data - data retrieved from the local storage
+         */
+        storeDataInLocalStorage: function (key, data) {
+            localStorage.setItem(key, JSON.stringify(data));
+        },
+
+        /**
+         * When we initialize the widget, we need to clear the stored data
+         * in the local storage
+         */
+        _clearStoredData: function () {
+            console.log(this.lsSelectedFilesKey);
+            localStorage.removeItem(this.lsSelectedFilesKey);
         }
     };
 });
